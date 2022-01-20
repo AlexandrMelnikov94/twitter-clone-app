c3b7b33870d400641cac4cd9efbe234aa8fff8d3
AlexandrMelnikov94 <76219280+AlexandrMelnikov94@users.noreply.github.com>
2022-01-20 13:15:16 +0300

    add post to firebase

--- components/Input.js
+++ components/Input.js
@@ -34,14 +34,15 @@ function Input() {
     });
 
     const imageRef = ref(storage, `posts/${docRef.id}/image`);
+    console.log(imageRef);
 
     if (selectedFile) {
       await uploadString(imageRef, selectedFile, "data_url").then(async () => {
         const downloadURL = await getDownloadURL(imageRef);
         await updateDoc(doc(db, "posts", docRef.id), {
           image: downloadURL,
-        })
-      })
+        });
+      });
     }
 
     setLoading(false);
@@ -71,7 +72,8 @@ function Input() {
 
 
   return (
-    <div className={`border-b border-grey-100 p-3 flex space-x-3 overflow-y-scroll truncate`}>
+    <div className={`border-b border-grey-100 p-3 flex space-x-3 overflow-y-scroll 
+    truncate ${loading && "opacity-60"}`}>
       <img src="https://yt3.ggpht.com/yti/APfAmoFlXWaR3khH4L4y1GK0sZDCEsfKJQjH2a3nA0IkTg=s88-c-k-c0x00ffffff-no-rj-mo"
            alt=""
            className="h-11 w-11 rounded-full cursor-pointer"/>
@@ -87,7 +89,7 @@ function Input() {
 
           {selectedFile && (
             <div className="relative">
-              <div className="absolute w-8 h-8 bg-[#15191c] hover:bg-[#272c26]
+              <div className="absolute w-5 h-5 bg-[#15191c] hover:bg-[#272c26]
             bg-opacity-75 rounded-full flex items-center justify-center top-1 left-1
             cursor-pointer"
                    onClick={() => setSelectedFile(null)}>
@@ -99,45 +101,47 @@ function Input() {
             </div>
           )}
         </div>
+        {!loading && (
+          <div className="flex item-center justify-between pt-2.5">
+            <div className="flex items-center">
+              <div className="icon"
+                   onClick={() => filePickerRef.current.click()}>
+                <PhotographIcon className="h-[22px] text-[#1d9bf0]"/>
+                <input
+                  type="file"
+                  hidden
+                  onChange={addImageToPost}
+                  ref={filePickerRef}/>
+              </div>
 
-        <div className="flex item-center justify-between pt-2.5">
-          <div className="flex items-center">
-            <div className="icon"
-                 onClick={() => filePickerRef.current.click()}>
-              <PhotographIcon className="h-[22px] text-[#1d9bf0]"/>
-              <input
-                type="file"
-                hidden
-                onChange={addImageToPost}
-                ref={filePickerRef}/>
+              <div className="icon"
+                   onClick={() => setShowEmojis(!showEmojis)}>
+                <EmojiHappyIcon className="text-[#1d9bf0] h-[22px]"/>
+              </div>
+
+              {showEmojis && (
+                <Picker
+                  onSelect={addEmoji}
+                  style={{
+                    position: "absolute",
+                    marginTop: "465px",
+                    marginLeft: -40,
+                    maxWidth: "320px",
+                    borderRadius: "20px",
+                  }}/>
+              )}
             </div>
-
-            <div className="icon"
-                 onClick={() => setShowEmojis(!showEmojis)}>
-              <EmojiHappyIcon className="text-[#1d9bf0] h-[22px]"/>
-            </div>
-
-            {showEmojis && (
-              <Picker
-                onSelect={addEmoji}
-                style={{
-                  position: "absolute",
-                  marginTop: "465px",
-                  marginLeft: -40,
-                  maxWidth: "320px",
-                  borderRadius: "20px",
-                }}/>
-            )}
-          </div>
-          <button
-            className="bg-[#1d9bf0] text-white rounded-full px-4 py-1.5
+            <button
+              className="bg-[#1d9bf0] text-white rounded-full px-4 py-1.5
           font-bold shadow-md hover:bg-[#1a8cd8] disabled:hover:bg-[#1d9bf0]
           disabled:opacity-50 disabled:cursor-default"
-            disabled={!input.trim() && !selectedFile}
-            onClick={sendPost}
-          >Tweet
-          </button>
-        </div>
+              disabled={!input.trim() && !selectedFile}
+              onClick={sendPost}
+            >Tweet
+            </button>
+          </div>
+        )}
+
       </div>
     </div>
 
